fddf8e58b680ff348749782cabbb4bb968d55066
bettingers <95344896+bettingers@users.noreply.github.com>
2022-10-24 15:24:14 +0200

    Reponse.php

--- src/Model/DataObject/Reponse.php
+++ src/Model/DataObject/Reponse.php
@@ -1,8 +1,71 @@
 <?php
 
 namespace App\Covoiturage\Model\DataObject;
+use App\Covoiturage\Model\DataObject\Question;
+use App\Covoiturage\Model\Repository\DatabaseConnection;
 
 class Reponse
 {
+
+    private string $id;
+    private string $id_utilisateur;
+    private Question $id_question;
+
+
+    public function __construct(string $id, string $id_utilisateur, Question $id_question)
+    {
+        $this->id = $id;
+        $this->id_utilisateur = $id_utilisateur;
+        $this->id_question = $id_question;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdUtilisateur(): string
+    {
+        return $this->id_utilisateur;
+    }
+
+    /**
+     * @param string $id_utilisateur
+     */
+    public function setIdUtilisateur(string $id_utilisateur): void
+    {
+        $this->id_utilisateur = $id_utilisateur;
+    }
+
+    /**
+     * @return Question
+     */
+    public function getIdQuestion(): Question
+    {
+        return $this->id_question;
+    }
+
+    /**
+     * @param Question $id_question
+     */
+    public function setIdQuestion(Question $id_question): void
+    {
+        $this->id_question = $id_question;
+    }
 
 }
