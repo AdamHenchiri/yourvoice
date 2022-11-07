529456e733036b80662443a5f030fd11ebebfb36
RathierS <rathier.sylia@gmail.com>
2022-11-07 23:02:50 +0100

    modif create section

--- src/View/section/created.php
+++ src/View/section/created.php
@@ -1,2 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Créer Section</title>
+</head>
+<body>
 <?php
 echo "section bien ajouter";
+?>
+<p>
+<a href="frontController.php?controller=section&action=create">Ajouter une section</a>
+</p>
+<p>
+    <a href="frontController.php?controller=question&action=list">Finir et voir la question </a>
+</p>
+</body>
