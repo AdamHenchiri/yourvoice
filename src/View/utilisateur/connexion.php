535bcef807c9eeaa407d27942be5b28bf765fac6
Kim Harribaud <kim.harribaud@etu.umontpellier.fr>
2022-11-09 13:49:26 +0100

    css

--- src/View/utilisateur/connexion.php
+++ src/View/utilisateur/connexion.php
@@ -9,7 +9,7 @@
     <div class="container_connexion">
             <h1>Connexion</h1>
             <input class="text" type="text" placeholder="Login*" name="login" id="login_id" required/>
-            <input class=text" type="password" placeholder="Mot de passe*" name="mdp" id="mdp" required/>
+            <input class="text" type="password" placeholder="Mot de passe*" name="mdp" id="mdp" required/>
             <p id="champsrequis">* champs requis</p>
             <input id="envoyer" type="submit" value="Connexion"/>
 
