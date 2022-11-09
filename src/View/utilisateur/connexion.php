97cf62a0595e766e3c216c29be8c765168b300fb
Kim Harribaud <kim.harribaud@etu.umontpellier.fr>
2022-11-09 13:32:32 +0100

    css

--- src/View/utilisateur/connexion.php
+++ src/View/utilisateur/connexion.php
@@ -6,17 +6,12 @@
 </head>
 <div class="container_connexion">
 <form method="post" action="frontController.php?controller=utilisateur&action=connected">
-
-        <legend>Connexion</legend>
-        <p>
+            <h1>Connexion</h1>
             <input type="text" placeholder="Login*" name="login" id="login_id" required/>
-        </p>
-        <p>
             <input type="password" placeholder="Mot de passe*" name="mdp" id="mdp" required/>
-        </p>
-        <p>
+            <p>* champs requis</p>
             <input id="envoyer" type="submit" value="Connexion"/>
-        </p>
+
 
 </form>
 <?php echo "<a href=\"frontController.php?controller=utilisateur&action=create\"> vous n'avez pas encore de compte ? cliquer ici!</a> "; ?>
