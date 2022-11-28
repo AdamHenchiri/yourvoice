25e0c527d55f2e1faf6ac2bfa09b04c6da7089bc
bettingers <95344896+bettingers@users.noreply.github.com>
2022-11-29 00:26:47 +0100

    GenericController

--- src/Controller/GenericController.php
+++ src/Controller/GenericController.php
@@ -6,6 +6,7 @@ class GenericController
 {
 
     protected static function afficheVue(string $cheminVue, array $parametres = []) : void {
+        //$parametres["msgFlash"] = MessageFlash::lireTousMessages();
         extract($parametres); // Crée des variables à partir du tableau $parametres
         require "../src/View/$cheminVue"; // Charge la vue
     }
